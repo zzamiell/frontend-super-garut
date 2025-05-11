bb4f9728fcdab5153d3698666b8fc9568d0fd674
abdaiii <anugrah.abdi@transtrack.id>
2025-05-11 12:46:34 +0700

    buat contohpenggunaan page with params

---
+++ pages/product/[id].vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+onMounted(() => {
+  console.log('Product ID:', route.params.id)
+})
+</script>
+
+<template>
+  <div>
+    <h1>Product Details</h1>
+    <p>Product ID: {{ route.params.id }}</p>
+  </div>
+</template>
